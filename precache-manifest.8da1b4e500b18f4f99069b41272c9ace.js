3570650cd47cf885a1bc244b85d32a8cd5dde1ee
Carmona-Rodriguez, Carmen <Carmen.Carmona-Rodriguez@capco.com>
2020-04-28 13:10:53 +0100

    Updates

---
+++ precache-manifest.8da1b4e500b18f4f99069b41272c9ace.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ceaf19f6627304cc3ebcb5a46f2b12",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "dfbca0ff3035a513f7a5",
+    "url": "/monsters/static/css/main.d942ff73.chunk.css"
+  },
+  {
+    "revision": "5eed07350c569f900072",
+    "url": "/monsters/static/js/2.38ca39a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.38ca39a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfbca0ff3035a513f7a5",
+    "url": "/monsters/static/js/main.342b244c.chunk.js"
+  },
+  {
+    "revision": "91d675f01e537b27d9b1",
+    "url": "/monsters/static/js/runtime-main.08530e1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
